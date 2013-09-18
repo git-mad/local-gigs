7a2993e01feb6e409b76a2a48e55d52797c101cd
Alex <alexbettadapur@gmail.com>
2013-09-17 23:24:30 -0400

    Lifecycle code. Includes a text box and intent extra functionality

---
+++ local-gigs_app/src/com/gitmad/local_gigs/HomeActivity.java
@@ -0,0 +1,46 @@
+package com.gitmad.local_gigs;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class HomeActivity extends Activity implements View.OnClickListener{
+
+
+    Button button;
+    EditText textBox;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.home_activity);
+
+        button = (Button)findViewById(R.id.button);
+        textBox = (EditText)findViewById(R.id.textBox);
+        button.setOnClickListener(this);
+
+
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.hello_world, menu);
+		return true;
+	}
+
+    @Override
+    public void onClick(View v)
+    {
+        //get text from text box, start activity
+        String text = textBox.getText().toString();
+        Intent i = new Intent(this, TextActivity.class);
+        i.putExtra("My Text", text);
+        startActivity(i);
+
+    }
+}
---
+++ local-gigs_app/src/com/gitmad/local_gigs/TextActivity.java
@@ -0,0 +1,37 @@
+package com.gitmad.local_gigs;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 9/17/13
+ * Time: 11:14 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class TextActivity extends Activity implements View.OnClickListener
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.text_activity);
+        Intent intent = getIntent();
+        String text = intent.getStringExtra("My Text");
+        TextView textBox = (TextView)findViewById(R.id.displayBox);
+        textBox.setText(text);
+
+        Button button = (Button)findViewById(R.id.closebutton);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        this.finish();
+    }
+}
